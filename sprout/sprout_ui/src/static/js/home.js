933b927374150520b2587eb622bdb2aef0107872
a-deep97 <a.deep97@outlook.com>
2023-12-22 01:12:35 +0530

    added logo component

--- sprout/sprout_ui/src/static/js/home.js
+++ sprout/sprout_ui/src/static/js/home.js
@@ -4,12 +4,14 @@ import React from 'react';
 import LeftPanel from './utilities/left_panel';
 import RightPanel from './utilities/right_panel'
 import PostCard from './utilities/post_card';
+import Logo from './utilities/logo';
 
 const HomePage = () => {
   return (
     <div className='page-body'>
         <LeftPanel/>  
         <div className="main-container">
+            <Logo/>
             <main class="col-md-9 ml-sm-auto col-lg-10 px-md-4 main-content">
                 <PostCard/>
             </main>
---
+++ sprout/sprout_ui/src/static/js/utilities/logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = () => {
+  return (
+    <div className='logo'>
+        <h2>Sprout</h2>
+    </div>
+  );
+};
+
+export default Logo;

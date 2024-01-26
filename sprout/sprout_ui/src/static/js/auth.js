384230ea9a8172a14b54651c48bfd9910f9ed83f
a-deep97 <a.deep97@outlook.com>
2024-01-27 00:45:52 +0530

    Added a config for particles , particles component yet to be added

--- sprout/sprout_ui/src/static/js/auth.js
+++ sprout/sprout_ui/src/static/js/auth.js
@@ -4,6 +4,8 @@ import React, { useState } from 'react';
 import LoginForm from './forms/login';
 import SignupForm from './forms/signup';
 
+import AuthBackgroundParticleConfig from './config/particle_config';
+
 const AuthPage = (props) => {
   const [isSignUp, setIsSignUp] = useState(false);
   const authPicture = require('../media/auth-img.jpg');
@@ -17,20 +19,20 @@ const AuthPage = (props) => {
   };
   return (
     <div className='auth-page'>
-        <div className="auth-page-container">
-          {isSignUp ? (
-            <SignupForm onToggleForm={handleToggleForm} />
-            ) : (
-            <LoginForm onToggleForm={handleToggleForm} setLoggedIn = {props.setLoggedIn} />
-          )}
-        </div>
-        <div className='form-image'>
-          <img
-              src= {authPicture}
-              width='100%'
-              height='100%'
-          />
-        </div>
+      <div className="auth-page-container">
+        {isSignUp ? (
+          <SignupForm onToggleForm={handleToggleForm} />
+          ) : (
+          <LoginForm onToggleForm={handleToggleForm} setLoggedIn = {props.setLoggedIn} />
+        )}
+      </div>
+      <div className='form-image'>
+        <img
+            src= {authPicture}
+            width='100%'
+            height='100%'
+        />
+      </div>
     </div>
   );
 };
